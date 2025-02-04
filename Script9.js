4145e8602f911c7c37942342bdc922348607fa05
ggaauurraavv <147307606+ggaauurraavv@users.noreply.github.com>
2025-02-04 22:23:00 +0530

    Script9.js

--- Script9.js
+++ Script9.js
@@ -4,12 +4,13 @@ function formatText(command) {
 
 function downloadDocument() {
     const editorContent = document.getElementById('editor').innerHTML;
-    const blob = new Blob([editorContent], { type: 'text/html' });
+    const blob = new Blob(['<html><body>' + editorContent + '</body></html>'], { type: 'application/msword' });
     const link = document.createElement('a');
     link.href = URL.createObjectURL(blob);
-    link.download = 'document.html';
+    link.download = 'document.doc';
     link.click();
 }
+
 
 document.getElementById('Home').addEventListener('click', function() {
     window.location.href = 'index.html';
